6f57e9b6e1ff680dd08aedfa1914c182571e4c2b
JIASHI CHEN <anderson@dhcp-10-202-144-112.tamulink.tamu.edu>
2015-05-07 22:52:08 -0500

    second commit

--- test.py
+++ test.py
@@ -2,22 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 def main():
     classification = [[],[],[],[],[],[],[],[],[],[]]
-    feature = [[],[],[],[],[],[],[],[],[],[]]
     f = open('modifiedInput.txt')
     for line in f:
         line = line.rstrip('\n')
         data = line.split(',')
         data = map(int,data)
         classification[data[-1]].append(data)
-        
-    for classnum in xrange(10):
-        for dataline in classification[classnum]:
-            feature[classnum].append(dataline[4])
+    for i in xrange(8):
+        feature = [[],[],[],[],[],[],[],[],[],[]]
+        for classnum in xrange(10):
+            for dataline in classification[classnum]:
+                feature[classnum].append(dataline[i])
     
-    for classnum in xrange(10):
-        plt.subplot(4,3,classnum+1)
-        plt.xlim(-1,13)
-        plt.hist(feature[classnum])
+        for classnum in xrange(10):
+            plt.figure(i)
+            plt.subplot(4,3,classnum+1)
+            plt.xlim(-1,13)
+            plt.hist(feature[classnum])
+    
     plt.show()
             
         
